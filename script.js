9c1cad10bb044030c723ee7b1501e7e20c9ba14c
romulo <romulodmilani@gmail.com>
2023-03-31 10:36:31 -0300

    basic calculator functions and operators

---
+++ script.js
@@ -0,0 +1,35 @@
+//BASIC MATH OPERATIONS
+function add(a, b) {
+    return a + b
+};
+
+function subtract(a, b) {
+    return a - b
+};
+
+function multiply(a, b) {
+    return a * b
+};
+
+function divide(a, b) {
+    return a / b
+};
+
+//OPERATE FUNCION
+function operate(operator, firstNumber, secondNumber) {
+    if (operator == '+') {
+        return add(firstNumber, secondNumber);
+    } else if (operator == '-') {
+        return subtract(firstNumber, secondNumber);
+    } else if (operator == '*') {
+        return multiply(firstNumber, secondNumber);
+    } else if (operator == '/') {
+        return divide(firstNumber, secondNumber);
+    };
+}
+
+
+//OPERATION VARIABLES
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
