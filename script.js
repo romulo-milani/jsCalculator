fa28e8b3c3cb25b0cabbf200bbbd9bb3d5e98b5e
romulo <romulodmilani@gmail.com>
2023-04-03 10:57:09 -0300

    terminada a lógica básica

--- script.js
+++ script.js
@@ -28,8 +28,77 @@ function operate(operator, firstNumber, secondNumber) {
     };
 }
 
+//FUNCTION THAT ERASES THE P ELEMENT
+function eraseP() {
+
+}
+
 
 //OPERATION VARIABLES
-let firstNumber = 0;
-let secondNumber = 0;
+let firstNumber = '';
+let secondNumber = '';
 let operator = '';
+
+//STORE THE NUMBERS ON CLICK IN A VARIABLE AND DISPLAY THEM
+const numberBtns = document.querySelectorAll('.numberBtn');
+const firstP = document.querySelector('.displayFirstP');
+const secondP = document.querySelector('.displaySecondP');
+
+numberBtns.forEach(numberBtn => {
+    numberBtn.addEventListener('click', (e) => {
+
+        if (operator != '') {
+
+            firstP.innerHTML = '';
+            secondP.innerHTML += e.target.innerHTML;
+            secondNumber = secondP.innerHTML;
+
+        } else {
+            firstP.innerHTML += e.target.innerHTML;
+            firstNumber = firstP.innerHTML;
+        }
+    })
+});
+
+//STORE THE OPERATOR IN A VARIABLE AND DISPLAY IT
+const operatorBtns = document.querySelectorAll('.operatorBtn');
+const operatorP = document.querySelector('.operatorP');
+const pScreenUp = document.querySelector('.pScreenUp');
+const screenUpDiv = document.querySelector('.screenUp');
+const screenDown = document.querySelector('.screenDown');
+
+operatorBtns.forEach(operatorBtn => {
+    operatorBtn.addEventListener('click', (e) => {
+        //copy the text and display it on top of the screen
+        firstNumber = firstP.innerHTML;
+        pScreenUp.innerHTML = firstNumber;
+
+        //Display the selected operator
+        operatorP.innerHTML = e.target.innerHTML;
+        operator = operatorP.innerHTML
+
+    })
+})
+
+//EQUALS BUTTON
+const equals = document.querySelector('.btnE');
+equals.addEventListener('click', function () {
+
+    if (firstNumber == '' || secondNumber == '' || operator == '') {
+
+    } else {
+
+        firstNumber = parseFloat(firstNumber);
+        secondNumber = parseFloat(secondNumber);
+
+        let result = operate(operator, firstNumber, secondNumber);
+
+        firstNumber = '';
+        secondNumber = '';
+        operator = '';
+        firstP.innerHTML = result;
+        pScreenUp.innerHTML = result;
+        secondP.innerHTML = '';
+        operatorP.innerHTML = '';
+    }
+})
